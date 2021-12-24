fe89746028bd4b7a9329046ed81b10b84965c9a9
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-12-25 00:42:26 +0330

    fix some bug

--- pkg/http/client.go
+++ pkg/http/client.go
@@ -3,10 +3,10 @@ package http
 import "github.com/go-resty/resty/v2"
 
 type Client interface {
-	Get(url string, query map[string]interface{}, header map[string]string) (*resty.Response, error)
-	Post(url string, body map[string]interface{}, header map[string]string) (*resty.Response, error)
-	Put(url string, body map[string]interface{}, header map[string]string) (*resty.Response, error)
-	Patch(url string, body map[string]interface{}, header map[string]string) (*resty.Response, error)
-	Delete(url string, body map[string]interface{}, header map[string]string) (*resty.Response, error)
-	Request(method string, url string, body map[string]interface{}, header map[string]string) (*resty.Response, error)
+	Get(url string, query interface{}, header map[string]string) (*resty.Response, error)
+	Post(url string, body interface{}, header map[string]string) (*resty.Response, error)
+	Put(url string, body interface{}, header map[string]string) (*resty.Response, error)
+	Patch(url string, body interface{}, header map[string]string) (*resty.Response, error)
+	Delete(url string, body interface{}, header map[string]string) (*resty.Response, error)
+	Request(method string, url string, body interface{}, header map[string]string) (*resty.Response, error)
 }
