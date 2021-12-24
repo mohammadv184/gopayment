91675bc59c6d569bd9b1b193e4545231f30f537f
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-12-25 00:41:59 +0330

    fix some bugs

--- traits/detail.go
+++ traits/detail.go
@@ -3,24 +3,24 @@ package traits
 import "fmt"
 
 type HasDetail struct {
-	Details map[string]string
+	details map[string]string
 }
 
 func (i *HasDetail) Detail(key string, value string) {
-	if i.Details == nil {
-		i.Details = make(map[string]string)
+	if i.details == nil {
+		i.details = make(map[string]string)
 	}
-	i.Details[key] = value
+	i.details[key] = value
 }
 func (i *HasDetail) GetDetail(key string) (string, error) {
-	if i.Details == nil {
+	if i.details == nil {
 		return "", fmt.Errorf("no details")
 	}
-	if value, ok := i.Details[key]; ok {
+	if value, ok := i.details[key]; ok {
 		return value, nil
 	}
 	return "", fmt.Errorf("not found detail")
 }
 func (i *HasDetail) GetDetails() map[string]string {
-	return i.Details
+	return i.details
 }
