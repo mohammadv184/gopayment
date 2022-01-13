f4c2bdee7c4299548eedf3e0bfcb1c57aef4a037
Mohammad Abbasi <mohammad.v184@gmail.com>
2022-01-13 19:16:11 +0330

    add zibal purchase step

---
+++ gateway/zibal/purchase.go
@@ -0,0 +1,54 @@
+package zibal
+
+import (
+	"encoding/json"
+	e "github.com/mohammadv184/gopayment/errors"
+	"strconv"
+
+	"github.com/mohammadv184/gopayment/helpers"
+
+	"github.com/mohammadv184/gopayment/invoice"
+)
+
+// Purchase sends a request to zibal to purchase an invoice.
+func (d *Driver) Purchase(invoice *invoice.Invoice) (string, error) {
+	var reqBody = map[string]interface{}{
+		"merchant":    d.Merchant,
+		"callbackUrl": d.Callback,
+		"description": invoice.GetDescription(),
+		"orderId":     invoice.GetUUID(),
+		"amount":      invoice.GetAmount(),
+	}
+	if d := invoice.GetDetail("phone"); d != "" {
+		reqBody["mobile"] = d
+	}
+	resp, _ := client.Post(APIPurchaseURL, reqBody, nil)
+	var res map[string]interface{}
+	err := json.Unmarshal(resp.Body(), &res)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode() != 200 || res["result"].(float64) != 100 {
+		return "", e.ErrPurchaseFailed{
+			Message: res["message"].(string),
+		}
+	}
+
+	return strconv.Itoa(int(res["trackId"].(float64))), nil
+}
+
+// PayURL returns the url to redirect the user to in order to pay the invoice.
+func (d *Driver) PayURL(invoice *invoice.Invoice) string {
+	return APIPaymentURL + invoice.GetTransactionID()
+}
+
+// PayMethod returns the Request Method to be used to pay the invoice.
+func (d *Driver) PayMethod() string {
+	return "GET"
+}
+
+// RenderRedirectForm renders the html form for redirect to payment page.
+func (d *Driver) RenderRedirectForm(invoice *invoice.Invoice) (string, error) {
+	return helpers.RenderRedirectTemplate(d.PayMethod(), d.PayURL(invoice), nil)
+}
