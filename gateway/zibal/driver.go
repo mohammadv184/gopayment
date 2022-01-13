14b6bd07e42b59b8c9ff33449ade91741bedddae
Mohammad Abbasi <mohammad.v184@gmail.com>
2022-01-13 19:15:56 +0330

    create zibal gateway

---
+++ gateway/zibal/driver.go
@@ -0,0 +1,34 @@
+package zibal
+
+import (
+	httpClient "github.com/mohammadv184/gopayment/pkg/http"
+)
+
+// Driver config struct for zibal driver
+type Driver struct {
+	Merchant string `json:"merchant"`
+	Callback string `json:"callback"`
+}
+
+// Const's for zibal driver
+const (
+	APIPurchaseURL = "https://gateway.zibal.ir/v1/request"
+	APIVerifyURL   = "https://gateway.zibal.ir/v1/verify"
+	APIPaymentURL  = "https://gateway.zibal.ir/start/"
+)
+
+var client httpClient.Client
+
+func init() {
+	client = httpClient.NewHTTP()
+}
+
+// GetDriverName returns driver name
+func (d Driver) GetDriverName() string {
+	return "Zibal"
+}
+
+// SetClient sets the http client
+func (d Driver) SetClient(c httpClient.Client) {
+	client = c
+}
